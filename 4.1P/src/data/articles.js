1784c51ffb7a486e1379342e8c452504e27b03ae
Architbh007 <bhullararchit@gmail.com>
2025-08-01 10:41:32 +1000

    Add files via upload

---
+++ 4.1P/src/data/articles.js
@@ -0,0 +1,25 @@
+const articles = [
+  {
+    id: 1,
+    title: "Understanding React",
+    description: "e.g., React Basics",
+    author: "Manya",
+    image: "https://picsum.photos/id/1011/200/140",
+  },
+  {
+    id: 2,
+    title: "Exploring NodeJS",
+    description: "e.g., NodeJS",
+    author: "Archit",
+    image: "https://picsum.photos/id/28/200/140",
+  },
+  {
+    id: 3,
+    title: "React Hooks Explained",
+    description: "e.g., React Hooks",
+    author: "Dev",
+    image: "https://picsum.photos/id/1035/200/140",
+  },
+];
+
+export default articles;
